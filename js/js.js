d4fc3422b67a7c3be2a9e07eef86e3940e804b27
Ostap <prickly4528@gmail.com>
2016-10-06 00:05:34 +0300

    adding slider on test webpage, done/test-bug

--- js/js.js
+++ js/js.js
@@ -1,1 +1,25 @@
-$('.sl').slick();
+$('.sl').slick({
+autoplay: true,
+autoplaySpeed:10000,
+cssEase:'ease-in',
+asNavFor: '.sl2',
+});
+$('.sl2').slick({
+  slidesToShow: 7,
+  centerMode: true,
+
+  dots: true,
+  asNavFor: '.sl',
+  focusOnSelect: true,
+  	arrows: false,
+ responsive: [
+{
+	breakpoint:900,
+	settings: {
+		 slidesToShow: 3,
+		 
+				}
+}
+]
+});
+	
