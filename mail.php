de14aed81bd43cca1040bf395e1d450ac25166a8
Ostap <prickly4528@gmail.com>
2016-11-05 13:05:12 +0200

    degub z-index for navigation in mobile devices

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
+
 <?php
-
-$recepient = "ostapukrainian@ukr.net";
-$sitename = "MESSAGE FRO OSTAP HETMAN WEBSITE";
-
-$name = trim($_POST["name"]);
-$mail = trim($_POST["mail"]);
-$subject = trim($_POST["subject"]);
-$message = "Name: $name \nEmail: $mail \nSubject: $subject";
-
-$pagetitle = "New feedback \"$sitename\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+    $name = $_POST['name'];
+    $mail = $_POST['mail'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $from = 'From: OSTAP HETMAN WEBSITE'; 
+    $to = 'ostapukrainian@ukr.netn.com'; 
+   
+			
+   $body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
+  ?>
